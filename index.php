afd65dd21dbd04f3e989134665ebc9330569f50a
slavko1989 <slavkoslave1989@gmail.com>
2023-04-21 00:21:01 +0200

    java script,html,css

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="color">
+	<span id="hex">#e74c3c</span>
+	<button onclick="getNewColor()">Get New Color</button>
+</div>
+
+<script src="script.js"></script>
+</body>
+</html>
